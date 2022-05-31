03ff991c93dba016c2338a7adc49063fdf4b8d20
Vladimir <pudgerikivich@gmail.com>
2022-05-31 15:49:54 +0300

    Изменения

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-	print(i)
-	return "Hello World!"
+def index():
+	return render_template("index.html", name="Jim")
 
 @app.route("/user/<id>/")
 def user_profile(id):
